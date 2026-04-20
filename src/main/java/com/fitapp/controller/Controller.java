e3c830234e20a7d5302f75a6404fa34cbef74484
pettelkr <118444937+pettelkr@users.noreply.github.com>
2026-04-20 14:52:07 +0200

    Delete src/main/java/com/fitapp/controller directory

--- src/main/java/com/fitapp/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.fitapp.controller;
-
-import com.fitapp.navigation.Navigator;
-
-public interface Controller {
-    public void changeView(String fxmlFile);
-    public void setNavigator(Navigator navigator);
-}
